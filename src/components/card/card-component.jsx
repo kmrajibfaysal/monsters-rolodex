cebdbe40b04039f0ea3349a2f83504a15a63a1c1
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-09-29 03:02:29 +0600

    finalizing app

---
+++ src/components/card/card-component.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import './card.styles.css';
+
+const Card = (props) => (
+    <div className="card-container">
+        <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster" />
+        <h2> {props.monster.name}</h2>
+        <p> {props.monster.email} </p>
+    </div>
+);
+
+export default Card;
